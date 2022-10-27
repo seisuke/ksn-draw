cc2085cd83ad84010878efe699cda45fba2949c8
seisuke <seisuke@ymail.com>
2022-10-27 19:23:06 +0900

    use renderComposable

--- src/jsMain/kotlin/main.js.kt
+++ src/jsMain/kotlin/main.js.kt
@@ -1,19 +1,25 @@
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.ui.window.Window
 import ksn.App
 import ksn.InitElementAndLocalProvideThat
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.renderComposable
 import org.jetbrains.skiko.wasm.onWasmReady
 
+
 fun main() {
+
+    renderComposable(rootElementId = "root") {
+        Div {
+            Text("abc")
+        }
+    }
+
     onWasmReady {
         Window("ksn-draw") {
             InitElementAndLocalProvideThat {
                 Column {
-                    OutlinedTextField(
-                        value = "abc",
-                        onValueChange = {}
-                    )
                     App()
                 }
             }
